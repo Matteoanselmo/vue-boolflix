d17409720d440f23658e421d43dada0a816091a5
Matteo Anselmo <matteo.anselmo96@gmail.com>
2022-04-01 18:00:13 +0200

    Aggiunte serie tv

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,40 +1,65 @@
 <template>
 <div class="px-4 pb-4">
   <input type="text" class="farm-control shadow-lg mb-4" placeholder="Cerca Film/Serie" v-model="filmToSearch">
+  <span class="flag-icon flag-icon-squared flag-icon-uk"></span>
   <div class="d-flex flex-wrap" v-if="(filmToSearch != '')">
     <FilmCard
-    :film="element"
     v-for="(element, index) in fileteredFilms" :key="index"
+    :film="element"
+    />
+    <SerieCard 
+    v-for="(serie, index) in fileteredSeries" :key="index"
+    :serie="serie"
     />
   </div>
+  
 </div>
 </template>
 
 <script>
 import axios from 'axios'
 import FilmCard from './FilmCard.vue'
+import SerieCard from './SerieCard.vue'
+import FlagIcon from 'vue-flag-icon'
+import Vue from 'vue';
+Vue.use(FlagIcon);
 export default {
   name: 'IndexMain',
   components: {
     FilmCard,
+    SerieCard
   },
   data: function(){
     return{
-      mainCards: null,
+      mainFilmCards: null,
+      mainSerieCards: null,
       filmToSearch: '',
       filmFilter: null,
+      serieFilter: null,
     }
   },
   created: function(){
-    this.getApiInfo();
+    this.getFilmInfo();
+    this.getSerieInfo();
   },
   methods: {
-      getApiInfo() {
+      getFilmInfo() {
         axios
       .get('https://api.themoviedb.org/3/movie/top_rated?api_key=291efde9ec311b454fd9de73cb4e8f5c&language=en-US')
       .then((result) =>{
-        this.mainCards = result.data.results;
-        console.log(this.mainCards);
+        this.mainFilmCards = result.data.results;
+        console.log(this.mainFilmCards);
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+    },
+    getSerieInfo() {
+        axios
+      .get('https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=scrubs')
+      .then((result) =>{
+        this.mainSerieCards = result.data.results;
+        console.log(this.mainSerieCards);
       })
       .catch((error) => {
         console.error(error);
@@ -45,10 +70,19 @@ export default {
       fileteredFilms(){
         if ( this.filmToSearch.toLowerCase().trim() === ''){
                 console.warn('Stai cercando una stringa vuota');
-                return this.mainCards;
+                return this.mainFilmCards;
             }
-        return this.mainCards.filter(
+        return this.mainFilmCards.filter(
                 (element) => element.title.toLowerCase().includes(this.filmToSearch.toLowerCase().trim())
+      );
+    },
+    fileteredSeries(){
+        if ( this.filmToSearch.toLowerCase().trim() === ''){
+                console.warn('Stai cercando una stringa vuota');
+                return this.mainSerieCards;
+            }
+        return this.mainSerieCards.filter(
+                (element) => element.name.toLowerCase().includes(this.filmToSearch.toLowerCase().trim())
       );
     }
   }
--- src/components/SerieCard.vue
+++ src/components/SerieCard.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
         <div class="card text-white mb-3 mx-2 p-3 shadow-lg" style="width: 18rem;">
-            <img :src="country" class="card-img-top" :alt="film.title">
+            <img :src="country" class="card-img-top" :alt="serie.name">
             <div class="card-body">
-                <h5 class="card-title">{{ film.title }}</h5>
-                <h6 class="card-title">{{ film.original_title }}</h6>
-                <p class="card-text">{{ film.original_language }}</p>
-                <a href="#" class="btn btn-primary">{{ film.vote_average }}</a>
+                <h5 class="card-title">{{ serie.name }}</h5>
+                <h6 class="card-title">{{ serie.original_name }}</h6>
+                <p class="card-text">{{ serie.original_language }}</p>
+                <a href="#" class="btn btn-primary">{{ serie.vote_count }}</a>
             </div>
         </div>
     </div>
@@ -14,8 +14,8 @@
 
 <script>
 export default {
-    name: 'FilmCard',
-    props: { 'film' : Object },
+    name: 'SerieCard',
+    props: { 'serie' : Object },
     data: function(){
         return{
             country: ''
@@ -26,13 +26,13 @@ export default {
     },
     methods: {
         countryToSearch(){
-            if(this.film.original_language == 'ja'){
+            if(this.serie.original_language == 'ja'){
                 this.country = 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Flag_of_Japan.svg/280px-Flag_of_Japan.svg.png';
-            }else if (this.film.original_language == 'en'){
+            }else if (this.serie.original_language == 'en'){
                 this.country = 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Flag_of_the_United_Kingdom_%283-2_aspect_ratio%29.svg/1200px-Flag_of_the_United_Kingdom_%283-2_aspect_ratio%29.svg.png';
-            }else if (this.film.original_language == 'hi'){
+            }else if (this.serie.original_language == 'hi'){
                 this.country = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Flag_of_Hawaii.svg/1280px-Flag_of_Hawaii.svg.png';
-            }else if(this.film.original_language == 'ko'){
+            }else if(this.serie.original_language == 'ko'){
                 this.country = 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/Flag_of_South_Korea_%281945%E2%80%931948%29.svg/2560px-Flag_of_South_Korea_%281945%E2%80%931948%29.svg.png';
             }
         }
